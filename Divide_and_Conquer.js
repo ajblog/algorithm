5cf6f376a0260021242e092bddf75c8f8418e74b
Ali Jafarian <a.jafarian@revotech.com.au>
2024-07-28 18:34:51 +0330

    added a kth smallest item in an array problem

--- Divide_and_Conquer.js
+++ Divide_and_Conquer.js
@@ -1,3 +1,4 @@
+//QUESTION1
 // Given an array of integers,
 // find the maximum difference between two elements
 // such that the larger element appears after the smaller element in the array.
@@ -44,9 +45,7 @@ function maxDiffernece(arr) {
 
 // console.log(maxDiffernece([7, 9, 5, 6, 3, 2]));
 
-// Sure! Here's another interesting divide and conquer question
-// that is similar in difficulty to the previous one:
-
+//QUESTION2
 // Question:
 // Given an array of integers, find the maximum sum of a contiguous subarray
 // using a divide and conquer approach.
@@ -93,6 +92,7 @@ function findMaxSubArraySum(arr) {
 
 // console.log(findMaxSubArraySum([-3, 6, 2]));
 
+//QUESTION3
 // Given a list of n integers, write a function to find the k-th
 // smallest element in the list using a divide and conquer approach.
 
@@ -104,3 +104,49 @@ function findMaxSubArraySum(arr) {
 // Input: arr = [7, 10, 4, 3, 20, 15], k = 4
 
 // Output: 10 (The 4th smallest element in the list is 10)
+
+function partition(arr, low, high) {
+  let pivot = arr[low];
+  let i = low;
+  let j = high;
+
+  while (i < j) {
+    // Move `i` to the right until an element greater than the pivot is found
+    while (arr[i] <= pivot && i < high) {
+      i++;
+    }
+    // Move `j` to the left until an element less than the pivot is found
+    while (arr[j] > pivot && j > low) {
+      j--;
+    }
+    // Swap elements at i and j if they haven't crossed
+    if (i < j) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+  // Swap pivot with element at j
+  [arr[low], arr[j]] = [arr[j], arr[low]];
+
+  return j; // Return the index of the pivot
+}
+
+function getKthSmallest(arr, k) {
+  function quickSelect(arr, low, high, k) {
+    if (low <= high) {
+      let pivotPosition = partition(arr, low, high);
+
+      if (pivotPosition === k) {
+        return arr[pivotPosition];
+      } else if (pivotPosition > k) {
+        return quickSelect(arr, low, pivotPosition - 1, k);
+      } else {
+        return quickSelect(arr, pivotPosition + 1, high, k);
+      }
+    }
+    return -1;
+  }
+
+  return quickSelect(arr, 0, arr.length - 1, k - 1);
+}
+
+console.log(getKthSmallest([3, 2, -10, 1], 3));
