9fe07ea858ad3dbd97001c4b370ebd02fbb42367
ajafarian <a.jafarian@revotech.com.au>
2024-08-17 20:10:14 +0330

    added a new problem with the wrong time complexity

--- Dynamic_Programming.js
+++ Dynamic_Programming.js
@@ -139,3 +139,38 @@ function uniquePaths(m, n) {
 }
 
 // console.log(uniquePaths(3, 2));
+// You are given an m x n integer matrix points (0-indexed). Starting with 0 points, you want to maximize the number of points you can get from the matrix.
+
+// To gain points, you must pick one cell in each row. Picking the cell at coordinates (r, c) will add points[r][c] to your score.
+
+// However, you will lose points if you pick a cell too far from the cell that you picked in the previous row. For every two adjacent rows r and r + 1 (where 0 <= r < m - 1), picking cells at coordinates (r, c1) and (r + 1, c2) will subtract abs(c1 - c2) from your score.
+
+// Return the maximum number of points you can achieve.
+
+function maxPoints(points) {
+  let dp = Array.from({ length: points.length }, () =>
+    Array(points[0].length).fill(0)
+  );
+  for (let i = 0; i < points[0].length; i++) {
+    dp[0][i] = points[0][i];
+  }
+  for (let i = 1; i < points.length; i++) {
+    for (let j = 0; j < points[0].length; j++) {
+      // Calculate the maximum value for dp[i][j]
+      let dpPrevValue = -Infinity;
+      for (let k = 0; k < points[0].length; k++) {
+        dpPrevValue = Math.max(dpPrevValue, dp[i - 1][k] - Math.abs(j - k));
+      }
+      dp[i][j] = points[i][j] + dpPrevValue;
+    }
+  }
+  return Math.max(...dp[points.length - 1]);
+}
+
+console.log(
+  maxPoints([
+    [5, 2, 1, 2],
+    [2, 1, 5, 2],
+    [5, 5, 5, 0],
+  ])
+);
