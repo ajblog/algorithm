a4f3a0d05ef8dfdad36857e87600726a8844d3eb
ajafarian <a.jafarian@revotech.com.au>
2024-08-17 20:32:27 +0330

    added a new two pointer problem

--- Random.js
+++ Random.js
@@ -162,3 +162,26 @@ function findMaxAverage(nums, k) {
 }
 
 // console.log(findMaxAverage([5], 1));
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+function removeDuplicates(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  let k = 1; // Initialize the count of unique elements to 1
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[k - 1]) {
+      nums[k] = nums[i]; // Overwrite the next unique element
+      k++;
+    }
+  }
+
+  return k;
+}
+
+// console.log(removeDuplicates([1, 1, 2, 3, 4, 4, 6]));
