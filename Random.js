79636be5f69fa315507e6db361f8e8de96068655
ajafarian <a.jafarian@revotech.com.au>
2025-02-05 00:27:43 +0330

    added leetcodes question of the day

--- Random.js
+++ Random.js
@@ -1101,3 +1101,35 @@ var waysToSplitArray = function (nums) {
   }
   return correctSplits;
 };
+
+// Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+
+// A subarray is defined as a contiguous sequence of numbers in an array.
+
+// A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi  < numsi+1. Note that a subarray of size 1 is ascending.
+
+// Example 1:
+
+// Input: nums = [10,20,30,5,10,50]
+// Output: 65
+// Explanation: [5,10,50] is the ascending subarray with the maximum sum of 65.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function (nums) {
+  let curIndex = 0;
+  let curSum = nums[0];
+  let maxSum = curSum;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] < nums[i]) {
+      curSum += nums[i];
+      maxSum = Math.max(maxSum, curSum);
+    } else {
+      curSum = nums[i];
+    }
+  }
+  return maxSum;
+};
+
+console.log(maxAscendingSum([12, 17, 15, 13, 10, 11, 12]));
